65cc33f17a6fb84fa361842a65158319216f183c
ditasmer <ditasmer@gmail.com>
2020-08-05 14:34:20 +0200

    Formulario action

--- ajax_form_sincrono.php
+++ ajax_form_sincrono.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 	//inicializar variables
 	$error=$mensaje=$nombre=$apellidos=null;
 
@@ -16,7 +16,7 @@
 			$mensaje = "$nombre $apellidos";
 		}
 	}
-?>
+?> 
 <!DOCTYPE html>
 <html>
 <head>
@@ -26,18 +26,31 @@
 		label {width:120px;display: inline-block;}
 		form {width: 300px; border:1px solid grey;padding:20px;margin:auto;}
 	</style>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script type="text/javascript">
+		$(inicio)
+
+		function inicio(){
+			//activar listener boton enviar
+			$('#enviar').on('click', enviarFormulario)
+		}
+
+		function enviarFormulario(){
+			alert('enviar formulario')
+		}
+	</script>
 </head>
 <body>
-	<form action="#" method='post'>
+	<form>
 		<label>Nombre</label>
-		<input type="text" name='nombre' value="<?=$nombre?>"><br>
+		<input type="text" id='nombre'><br>
 		<label>Apellidos</label>
-		<input type="text" name='apellidos' value="<?=$apellidos?>"><br><br>
+		<input type="text" id='apellidos'><br><br>
 		<label></label>
-		<input type="submit" name='enviar'><br><br>
+		<input type="submit" id='enviar' value="Enviar"><br><br>
 		<label></label>
-		<textarea><?=$mensaje?></textarea><br>
-		<span><?=$error?></span>
+		<textarea id="mensaje"></textarea><br>
+		<span id="error"></span>
 	</form>
 </body>
 </html>
