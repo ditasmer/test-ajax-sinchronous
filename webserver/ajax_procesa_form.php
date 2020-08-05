416d8dd235e3b1b079773be53a4ea7ab3985530c
ditasmer <ditasmer@gmail.com>
2020-08-05 15:05:02 +0200

    Servicio procesa respuesta

---
+++ webserver/ajax_procesa_form.php
@@ -0,0 +1,22 @@
+ <?php
+	//inicializar variables
+	//$error=$mensaje=$nombre=$apellidos=null;
+
+	//comprobar si hemos pulsado el botón enviar
+	//if(isset($_POST['enviar'])) {
+		//recuperar la información del formulario
+		$nombre = trim($_POST['nombre']);
+		$apellidos = trim($_POST['apellidos']);
+
+		//validar que estén informados
+		if ($nombre=='' || $apellidos=='') {
+			$mensaje = 'nombre y apellidos obligatorios';
+		} else {
+			//confeccionar el mensaje de respuesta
+			$mensaje = "$nombre $apellidos";
+		}
+
+		//enviar mensaje de respuesta, tanto si es error como si no
+		echo $mensaje;
+	//}
+?> 
